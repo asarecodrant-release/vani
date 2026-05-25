c53dd0297640ed82676db1ce47f5f91729ab895e
asare.codrant-release <asare.codrant@gmail.com>
2026-05-26 03:17:36 +0530

    Chatbot appearance fix

--- embed.js
+++ embed.js
@@ -39,7 +39,7 @@
     border: "0",
     background: "transparent",
     colorScheme: "normal",
-    zIndex: "999999",
+    zIndex: "2147483647",
     transition: "width .28s cubic-bezier(.2,.8,.2,1), height .28s cubic-bezier(.2,.8,.2,1)"
   });
 
